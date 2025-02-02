f74fd5b2c123d09d9c5d6638c842fc6712e90bea
BrayanQuintero123 <brayan2221707@correo.uis.edu.co>
2025-02-01 20:27:19 -0500

    model_builder.py

---
+++ model_builder.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+class CNN(nn.Module):
+    """Creates the TinyVGG architecture.
+
+    Replicates the TinyVGG architecture from the CNN explainer website in PyTorch.
+    See the original architecture here: https://poloclub.github.io/cnn-explainer/
+
+    Args:
+    input_shape: An integer indicating number of input channels.
+    hidden_units: An integer indicating number of hidden units between layers.
+    output_shape: An integer indicating number of output units.
+    """
+    def __init__(self, input_shape, hidden_units, output_shape):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*32*32, out_features=output_shape),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        return x
